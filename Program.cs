a810974df3f63597013c9c480f66607e4c59fd08
Ajibade Ademola <Ajibadequwiyu@gmail.com>
2024-09-15 20:16:04 +0100

    CORS Enabled

--- Program.cs
+++ Program.cs
@@ -11,6 +11,19 @@ builder.Services.AddDbContext<FlavourWheelContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("FlavourWheelDatabase"),
     new MySqlServerVersion(new Version(8, 0, 23))));  // Adjust the version to match your MySQL server version
 
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,6 +38,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use CORS middleware
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
